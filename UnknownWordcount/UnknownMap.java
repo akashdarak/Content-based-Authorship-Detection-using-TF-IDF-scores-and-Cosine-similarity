5d345f86f089011ae8b08b5e881c5d3441397a3b
Akash Darak <akash.darak@colostate.edu>
2016-04-15 20:10:33 -0600

    Added files via upload

---
+++ UnknownWordcount/UnknownMap.java
@@ -0,0 +1,59 @@
+package package2;
+
+/*
+
+Author: Akash Darak
+Date: 03/25/2016
+
+*/
+
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.LongWritable;
+//import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Mapper;
+
+public class UnknownMap extends Mapper<LongWritable,Text,Text,IntWritable>{
+
+	private String author =null;
+	private String keys=null;
+	private String line=null;
+	private String[] str=null;
+	private Text word=new Text();
+	private final static IntWritable one = new IntWritable(1);
+	
+	public void map(LongWritable key,Text value,Context context) throws IOException, InterruptedException{
+		
+		try{
+			if((value.toString()).contains("<===>")) {
+				
+				str = (value.toString()).split("<===>");
+				author = str[0].toString().toLowerCase().replaceAll("[^a-zA-Z]"," ");
+				line=str[1].toString().toLowerCase().replaceAll("[^a-zA-Z]"," ");
+				StringTokenizer tokenizer = new StringTokenizer(line);
+			
+			while (tokenizer.hasMoreTokens())
+			{
+				String token = tokenizer.nextToken().replaceAll("[^a-zA-Z]","");
+				
+				if(token.length() != 0) {
+					keys = author + "\t" + token;
+					//Buffer = substring;
+					word.set(keys);
+					//context.write(word, new IntWritable(1));
+					context.write(word, one);
+				}
+	
+			}
+		
+			}
+		}catch(Exception e){
+			
+		}
+
+	}
+
+}
---
+++ UnknownWordcount/UnknownReduce.java
@@ -0,0 +1,35 @@
+package package2;
+
+/*
+
+Author: Akash Darak
+Date: 03/25/2016
+
+*/
+
+import java.io.IOException;
+//import java.util.Set;
+//import java.util.TreeSet;
+
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Reducer;
+
+	
+	  public class UnknownReduce extends Reducer<Text,IntWritable,Text,IntWritable> {
+		    private IntWritable result = new IntWritable();
+
+		    public void reduce(Text key, Iterable<IntWritable> values,
+		                       Context context
+		                       ) throws IOException, InterruptedException {
+		      int sum = 0;
+		      for (IntWritable val : values) {
+		        sum += val.get();
+		      }
+		      result.set(sum);
+		      context.write(key, result);
+		    }
+		  }
+
+
+
