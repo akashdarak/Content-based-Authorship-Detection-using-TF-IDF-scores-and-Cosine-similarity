449364be8ed1c14e660544d6d51b6e3b68340bfd
Akash Darak <akash.darak@colostate.edu>
2016-04-16 00:59:37 -0600

    Update MainUnknown.java

--- Cosine/MainUnknown.java
+++ Cosine/MainUnknown.java
@@ -2,12 +2,9 @@ package package2;
 
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.conf.Configuration;
-//import org.apache.hadoop.conf.Configured;
 import org.apache.hadoop.fs.Path;
-//import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.filecache.DistributedCache;
-//import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
@@ -34,14 +31,12 @@ public class MainUnknown {
 		job.setInputFormatClass(TextInputFormat.class);
 		job.setOutputFormatClass(TextOutputFormat.class);
 			
-		DistributedCache.addCacheFile(new Path(args[0]).toUri(), job.getConfiguration());		//Cosine
-		FileInputFormat.setInputPaths(job, new Path(args[1]));									//Cosine
-		FileOutputFormat.setOutputPath(job, new Path(args[2]));									//Cosine	
+		DistributedCache.addCacheFile(new Path(args[0]).toUri(), job.getConfiguration());		
 		
-		/*
-		FileInputFormat.setInputPaths(job, new Path(args[0]));
-		FileOutputFormat.setOutputPath(job, new Path(args[1]));
-		*/
+		FileInputFormat.setInputPaths(job, new Path(args[1]));						
+		FileOutputFormat.setOutputPath(job, new Path(args[2]));						
+
+		System.exit(job.waitForCompletion(true) ? 0 : 1);
 		
 		if(job.waitForCompletion(true))
 			System.out.println("Job Completed");
