4b017b00ae959c5cb6df6f5384bf212da749f28b
Akash Darak <akash.darak@colostate.edu>
2016-04-16 00:54:43 -0600

    Update Main.java

--- TFIDF/Main.java
+++ TFIDF/Main.java
@@ -1,20 +1,15 @@
 package package2;
 
 /*
-
 Author: Akash Darak
 Date: 03/20/2016
-
 */
 
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.conf.Configuration;
-//import org.apache.hadoop.conf.Configured;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.Text;
-
-//import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
@@ -28,30 +23,16 @@ public class Main {
 		Job job=Job.getInstance(conf);
 		job.setJarByClass(Main.class);
 		
-		
 		job.setMapperClass(TFIDFMap.class);
 		job.setReducerClass(TFIDFReduce.class);
 		
 		job.setOutputKeyClass(Text.class);
 		job.setOutputValueClass(IntWritable.class);
 		
-		
-		/*
-		job.setMapperClass(AAVMap.class);		//AAV
-		job.setReducerClass(AAVReduce.class);	//AAV
-		
-		job.setOutputKeyClass(Text.class);		//Unknown
-		job.setOutputValueClass(Text.class);			//Unknown
-		
-		
-		job.setInputFormatClass(TextInputFormat.class);
-		job.setOutputFormatClass(TextOutputFormat.class);
-		*/	
-		
 		FileInputFormat.setInputPaths(job, new Path(args[0]));
 		FileOutputFormat.setOutputPath(job, new Path(args[1]));
 
-		
+		System.exit(job.waitForCompletion(true) ? 0 : 1);
 	}
 
 }
