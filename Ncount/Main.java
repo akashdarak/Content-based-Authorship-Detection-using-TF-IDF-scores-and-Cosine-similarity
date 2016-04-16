3138085ffa29635655e8d2920a16e061b7d2bfe3
Akash Darak <akash.darak@colostate.edu>
2016-04-16 00:56:08 -0600

    Update Main.java

--- Ncount/Main.java
+++ Ncount/Main.java
@@ -2,12 +2,9 @@ package package2;
 
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.conf.Configuration;
-//import org.apache.hadoop.conf.Configured;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.Text;
-
-//import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
@@ -21,11 +18,6 @@ public class Main {
 		Job job=Job.getInstance(conf);
 		job.setJarByClass(Main.class);
 		
-		/*
-		job.setMapperClass(Map1.class);
-		job.setReducerClass(Reduce1.class);
-		*/
-		
 		job.setMapperClass(Map3.class);
 		job.setReducerClass(Reduce3.class);
 		
@@ -35,10 +27,11 @@ public class Main {
 		job.setInputFormatClass(TextInputFormat.class);
 		job.setOutputFormatClass(TextOutputFormat.class);
 			
-		
 		FileInputFormat.setInputPaths(job, new Path(args[0]));
 		FileOutputFormat.setOutputPath(job, new Path(args[1]));
 
+		System.exit(job.waitForCompletion(true) ? 0 : 1);
+		
 		if(job.waitForCompletion(true))
 			System.out.println("Job Completed");
 		else System.out.println("Job Failed");
